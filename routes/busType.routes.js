23d291880f8cfb0c2e385949edce003d240eaa76
Binuri-Karunathilake <binurikarunathilake@gmail.com>
2022-11-14 13:22:43 +0530

    Made changes to routes

--- routes/busType.routes.js
+++ routes/busType.routes.js
@@ -4,10 +4,15 @@ import { addBusType, adddBusType, deleteBusType, getAllBusTypes, getBusType } fr
 
 const router = Express.Router();
 
+// get all bus types route
 router.get('/', getAllBusTypes);
+// add bus types route
 router.post('/', addBusType);
+// get specific bus types route
 router.get('/:id', getBusType);
+// delete specific bus types route
 router.delete('/:id', deleteBusType);
+// add types route
 router.post('/addBusType', adddBusType);
 
 export default router;
